65ccbe1625343f2276824eb6a2d271fc8db0aa29
John <jbartlettdesign@gmail.com>
2021-05-13 18:22:20 -0500

    fixed file generation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const inquirer = require('inquirer');
 const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Engineer');
 
 new Employee().promptEmployeeStats();
-module.exports = indexer;
+
+
